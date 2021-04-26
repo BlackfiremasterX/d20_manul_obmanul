b4b01ddaad43e05fd3ba361e5752589e16916c0e
BlackfiremasterX <misharozental@gmail.com>
2021-04-26 17:35:05 +0700

    D20_Complete_manul

--- app/src/main/java/com/example/dnd_dice_d20/Roll_The_Dice.kt
+++ app/src/main/java/com/example/dnd_dice_d20/Roll_The_Dice.kt
@@ -17,11 +17,50 @@ class Roll_The_Dice : AppCompatActivity() {
     private var SpeedCount = 0
     private var soundPool: SoundPool? = null
     private var soundId1: Int = 0
+    private var soundId2: Int = 0
+    private var soundId3: Int = 0
+    private var soundId4: Int = 0
+    private var soundId5: Int = 0
+    private var soundId6: Int = 0
+    private var soundId7: Int = 0
+    private var soundId8: Int = 0
+    private var soundId9: Int = 0
+    private var soundId10: Int = 0
+    private var soundId11: Int = 0
+    private var soundId12: Int = 0
+    private var soundId13: Int = 0
+    private var soundId14: Int = 0
+    private var soundId15: Int = 0
+    private var soundId16: Int = 0
+    private var soundId17: Int = 0
+    private var soundId18: Int = 0
+    private var soundId19: Int = 0
+    private var soundId20: Int = 0
+
 
     fun playSound(sound: Int) {
         var soundId = 0
         when (sound) {
             1 -> soundId = soundId1
+            2 -> soundId = soundId2
+            3 -> soundId = soundId3
+            4 -> soundId = soundId4
+            5 -> soundId = soundId5
+            6 -> soundId = soundId6
+            7 -> soundId = soundId7
+            8 -> soundId = soundId8
+            9 -> soundId = soundId9
+            10 -> soundId = soundId10
+            11 -> soundId = soundId11
+            12 -> soundId = soundId12
+            13 -> soundId = soundId13
+            14 -> soundId = soundId14
+            15 -> soundId = soundId15
+            16 -> soundId = soundId16
+            17 -> soundId = soundId17
+            18 -> soundId = soundId18
+            19 -> soundId = soundId19
+            20 -> soundId = soundId20
             else -> print("Error!")
         }
         soundPool?.play(soundId, 1F, 1F, 0, 0, 1F)
@@ -34,7 +73,28 @@ class Roll_The_Dice : AppCompatActivity() {
 
 
         soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
-        soundId1 = soundPool!!.load(baseContext, R.raw.lebovsky, 1)
+        soundId1 = soundPool!!.load(baseContext, R.raw.one, 1)
+        soundId2 = soundPool!!.load(baseContext, R.raw.two, 1)
+        soundId3 = soundPool!!.load(baseContext, R.raw.three, 1)
+        soundId4 = soundPool!!.load(baseContext, R.raw.four, 1)
+        soundId5 = soundPool!!.load(baseContext, R.raw.five, 1)
+        soundId6 = soundPool!!.load(baseContext, R.raw.six, 1)
+        soundId7 = soundPool!!.load(baseContext, R.raw.seven, 1)
+        soundId8 = soundPool!!.load(baseContext, R.raw.eight, 1)
+        soundId9 = soundPool!!.load(baseContext, R.raw.nine, 1)
+        soundId10 = soundPool!!.load(baseContext, R.raw.ten, 1)
+        soundId11 = soundPool!!.load(baseContext, R.raw.eleven, 1)
+        soundId12 = soundPool!!.load(baseContext, R.raw.twelve, 1)
+        soundId13 = soundPool!!.load(baseContext, R.raw.thirteen, 1)
+        soundId14 = soundPool!!.load(baseContext, R.raw.fourteen, 1)
+        soundId15 = soundPool!!.load(baseContext, R.raw.fifteen, 1)
+        soundId16 = soundPool!!.load(baseContext, R.raw.sixteen, 1)
+        soundId17 = soundPool!!.load(baseContext, R.raw.seventeen, 1)
+        soundId18 = soundPool!!.load(baseContext, R.raw.eighteen, 1)
+        soundId19 = soundPool!!.load(baseContext, R.raw.nineteen, 1)
+        soundId20 = soundPool!!.load(baseContext, R.raw.twenty, 1)
+
+
 
         findViewById<ImageView>(R.id.dnd_dice).setOnClickListener {
 
@@ -112,13 +172,16 @@ class Roll_The_Dice : AppCompatActivity() {
                             RotationY++
                         }
                     }
+                }
 
-                }
-                if (SpeedCount < 0) {
-                    playSound(1)
-                }
+            playSound(RandomNumber)
+
+
             }
         }
+
+
+
 
     }
 }
